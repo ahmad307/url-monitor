2a4fd273f568f750aae65e8facb89e62b65435de
Ahmad <ahmad.hussain.307@gmail.com>
2022-04-21 20:56:31 +0200

    Added user session auth and user update

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const User = require('../models/users')
 
 exports.addUser = (req, res) => {
@@ -9,15 +10,38 @@ exports.addUser = (req, res) => {
         password: req.body.password
     })
     user.save((err, newUser) => {
-        if (err)
+        if (err){
             res.status(400).send(err);
-        else
+        }
+        else {
+            passport.authenticate("local");
             res.status(200).send(`User ${newUser.name} added`);
+        }
     })
 }
 
-exports.updateUser = (req, res) => {
+exports.updateUser = async (req, res) => {
+    const body = req.body;
+    const user = await User.findOne({email: body.email});
+    if (!user) {
+        return res.status(400).send('User not found');
+    }
 
+    if (body.name) {
+        user.name = body.name;
+    }
+    if (body.password) {
+        user.password = body.password;
+    }
+
+    user.save((err) => {
+        if (err) {
+            res.status(409).send(err);
+        }
+        else {
+            res.status(200).send();
+        }
+    })
 }
 
 exports.deleteUser = (req, res) => {
@@ -25,16 +49,30 @@ exports.deleteUser = (req, res) => {
 }
 
 exports.signIn = (req, res) => {
-    const {email, password} = req.body;
+    passport.authenticate("local", {}, (err, user, info) => {
+        if (err || !user) {
+            return res.status(400).send("User not found or incorrect password");
+        }
+        req.login(user, (err) => {
+            if (err) throw(err);
+        });
+        res.status(200).send();
+    })(req, res);
+}
+
+exports.authenticate = (email, password, done) => {
+    // TODO: handle non-existing user
     User.findOne({email: email},'password', async(err, user) => {
-        console.log("3");
-        if (err)  res.status(400).send(err);
+        if (err)  {
+            console.log('auth err');
+            return done(err, false);
+        }
         const isMatch = await user.comparePassword(password);
         if (isMatch)  {
-            res.status(200).send();
+            done(null, user);
         }
         else {
-            res.status(400).send("Incorrect password");
+            done(null, false);
         }
     })
 }
--- server.js
+++ server.js
@@ -6,22 +6,52 @@ const express = require('express');
 const app = express();
 const usersRouter = require('./routes/users');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+const userModel = require('./models/users');
+const userController = require('./controllers/users');
+const port = 3000;
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
-const port = 3000;
 
+// Set MongoDb connection
 mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection
 db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to MongoDb'));
 
-app.set('view engine', 'ejs');
+// Passport
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy({usernameField: 'email'}, userController.authenticate));
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Url Monitor");
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    userModel.findById(id, (err, user) => {
+        done(err, user);
+    });
 });
 
+// Routes
+app.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.send(`Welcome ${req.user.name}`);
+    }
+    else {
+        return res.send('Not welcome!');
+    }
+    
+});
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
